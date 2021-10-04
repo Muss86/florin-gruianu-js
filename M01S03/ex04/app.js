d25d19bfa30191577a1caf3fbe48bd22068fa206
Muss86 <florin.gruianu@gmail.com>
2021-10-04 21:55:00 +0300

    4 octombrie

---
+++ M01S03/ex04/app.js
@@ -0,0 +1,19 @@
+var myArray = [1, 2, 3, 4, 5, 6];
+console.warn(`Afiseaza numarul de pe pozitia 5 a arrayului`);
+console.log(myArray[5].toString());
+
+console.warn(`Afiseaza nr 3 din array`);
+console.log(myArray[2].toString());
+
+console.warn(`Afiseaza lungimea array`);
+console.log(myArray.length.toString());
+
+console.warn(`Afiseaza suma numerelor de pe pozitia 0 si 4.
+`);
+console.log((myArray[0] + myArray[4]).toString());
+
+console.warn(`Afiseaza pozitia 1`);
+console.log(myArray[1].toString());
+
+// console.warn(`Afiseaza numarul de pe pozitia 2 a arrayului.`);
+// console.log(myArray[1].toString());
