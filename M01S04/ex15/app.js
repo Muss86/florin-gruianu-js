2d2cbd4c09d21425e8b6aa8d3a3501c171f52664
Muss86 <florin.gruianu@gmail.com>
2021-10-20 19:11:43 +0300

    tema 1 18oct

--- M01S04/ex15/app.js
+++ M01S04/ex15/app.js
@@ -31,3 +31,22 @@ var person = {
     },
   ],
 };
+
+console.warn(
+  `Folosind fisierele de la exercitiul 06, si doua bucle for imbricate (nested)
+   afiseaza diferenta de varsta dintre fiecare membru al arrayului friends si ceilalti membri.`,
+);
+
+for (i = 0; i < person.friends.length; i++) {
+  for (j = 0; j < person.friends.length; j++) {
+    if (i !== j) {
+      console.log(
+        `Intre ${person.friends[i].name} si ${
+          person.friends[j].name
+        } este o diferenta de ${Math.abs(
+          person.friends[i].age - person.friends[j].age,
+        )} ani.`,
+      );
+    }
+  }
+}
--- M01S05/ex02/app.js
+++ M01S05/ex02/app.js
@@ -3,7 +3,7 @@ function calculateRectangleArea(L, l) {
 
   // return area;
 
-  return L * L;
+  return L * l;
 }
 
 console.warn(
@@ -18,7 +18,32 @@ console.warn(
 const rectangle1Surface = calculateRectangleArea(5, 3);
 const rectangle2Surface = calculateRectangleArea(3, 2);
 console.log(rectangle1Surface + rectangle2Surface);
+console.log(rectangle1Surface);
+console.log(rectangle2Surface);
 
 console.warn(
   `Calculeaza intr-o variabila numita squareSurface aria unui patrat de 3 pe 3 si afiseaza-i valoarea in consola`,
 );
+const squareSurface = calculateRectangleArea(3, 3);
+console.log(squareSurface);
+
+console.warn(
+  `Folosind functia calculateRectangleArea() afiseaza suprafata unui dreptunghi de 12 pe 9`,
+);
+const rectangleSurface = calculateRectangleArea(12, 9);
+console.log(rectangleSurface);
+
+console.warn(
+  `Afiseaza suprafata totala a trei dreptunghiuri de 15 pe 31,  8 pe 9, respectiv 10 pe 5. Foloseste variabile.`,
+);
+
+const rectangleASurface = calculateRectangleArea(15, 31);
+const rectangleBSurface = calculateRectangleArea(8, 9);
+const rectangleCSurface = calculateRectangleArea(10, 5);
+console.log(rectangleASurface + rectangleBSurface + rectangleCSurface);
+
+console.warn(
+  `Calculeaza intr-o variabila numita anotherSquare aria unui patrat de 8 pe 8`,
+);
+const anotherSquare = calculateRectangleArea(8, 8);
+console.log(anotherSquare);
