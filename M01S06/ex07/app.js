46ada887298fee36fa6d190ed8662a27c3b35092
Muss86 <florin.gruianu@gmail.com>
2021-10-30 00:20:15 +0300

    corectat let i si backtick

--- M01S06/ex07/app.js
+++ M01S06/ex07/app.js
@@ -9,8 +9,12 @@ const paragraphLog = function () {
     document.body.append(logContainer);
   }
 
+  let i; //asa ziceai sa pun let i?
   for (i = 0; i < arguments.length; i++) {
-    result += arguments[i] + ' ';
+    //pot sa pun si asa: for(let i =0; i < arguments.length; i++)?
+    result = `${result}  ${arguments[i]} `;
+    // asa trebuia cu backtick?
+    // result += arguments[i] + ` `;
   }
 
   messageElement.innerText = result;
