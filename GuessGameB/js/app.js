7c4275b0e908eda31866f2b98a48a944519aa93b
Muss86 <florin.gruianu@gmail.com>
2021-10-30 03:28:00 +0300

    guess game

--- GuessGameB/js/app.js
+++ GuessGameB/js/app.js
@@ -1,19 +1,37 @@
-// userNumber --- userMessage
-const userNumber = document.querySelector('.user-input');
-const form = document.querySelector('#form');
+const userNumber = document.getElementById('userNumber');
+const userMessage = document.getElementById('userMessage');
+const livesUser = document.getElementById('livesUser');
+const form = document.querySelector('form');
 const myNumber = Math.floor(Math.random() * 10 + 1);
 
-form.addEventListener('submit', function () {
-  let userGuess = Number(userNumber.value);
-  userNumber.value = '';
+form.addEventListener('submit', function (event) {
+  let userGuess = userNumber.value || 0;
+  let result = '';
+  let lives = 5;
 
-  if (userGuess == myNumber) {
-    displayText('Ai castigat');
-  } else if (userGuess > myNumber) {
-    displayText('Numarul e prea mare');
-  } else if (userGuess < myNumber) {
-    displayText('Numarul este prea mic');
-  } else {
-    displayText('Ai pierdut');
+  for (i = 5; i > 0; i--) {
+    if (userGuess == myNumber) {
+      result = 'Ai castigat';
+
+      break;
+      // console.log('Ai castigat');
+    } else if (userGuess > myNumber) {
+      result = 'Numarul e prea mare';
+      i--;
+      lives -= 1;
+      // console.log('Numarul e prea mare');
+    } else if (userGuess < myNumber) {
+      result = 'Numarul este prea mic';
+      i--;
+      lives -= 1;
+      // console.log('Numarul este prea mic');
+    } else {
+      result = 'Ai pierdut';
+      break;
+      // console.log('Ai pierdut');
+    }
   }
+  userMessage.innerText = result;
+  livesUser.innerText = lives;
+  event.preventDefault();
 });
