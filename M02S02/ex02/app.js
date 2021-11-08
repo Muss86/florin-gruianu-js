10cd4f937dcbe75040079c6cb8c4089d5a976d1b
Muss86 <florin.gruianu@gmail.com>
2021-11-08 22:38:56 +0200

    jQuery 8 noiembrie

--- M02S02/ex02/app.js
+++ M02S02/ex02/app.js
@@ -8,25 +8,162 @@
 
 // note the overloading
 $(function () {
-  $personForm = $('#personForm').on('submit', function (event) {
+  const $personForm = $('#personForm').on('submit', function (event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const person = {};
+    const person = {
+      skills: [],
+    };
 
     for (const fieldData of formData.entries()) {
       const [fieldName, fieldValue] = fieldData;
 
+      if (fieldName.startsWith('skill-')) {
+        person.skills.push(fieldValue);
+
+        continue;
+      }
+
       person[fieldName] = fieldValue;
     }
 
+    $('.personDetails').remove();
+
     const $personContainer = renderPerson(person);
-    // variation
-    $personContainer.appendTo($personForm);
+    $personContainer.insertAfter($personForm);
+
+    resetForm($personForm);
+    $('#personForm.skillsUl').remove();
+  });
+
+  const $skillInput = $('#skillInput');
+  // nextSiblingElement - DOM
+  $skillInput.next().on('click', function () {
+    // nu avem nevoie de event in cazul type='button'
+    const $skillButton = $(this);
+    const $skillInput = $skillButton.prev();
+    const skillName = $skillInput.val().trim(); //.value <- proprietate in DOM
+
+    if (skillName.length <= 0) {
+      return;
+    }
+
+    // DOM -> parentElement
+    const $parentFieldset = $skillButton.parent();
+    let $skillsUl = $parentFieldset.find('.skillsUl');
+
+    // runs olny once
+    if ($skillsUl.length === 0) {
+      // create skillsUl
+      $skillsUl = $('<ul>', {
+        class: 'skillsUl',
+      });
+
+      // jquery event delegation
+      $skillsUl.on('click', '.deleteSkillButton', function () {
+        //this -> deleteSkillButton
+        $(this).parent().remove();
+      });
+
+      $skillsUl.on('click', '.editSkillButton', function () {
+        const $editButton = $(this).hide();
+
+        $editButton.siblings('input[name^="skill-"]').attr('type', 'text');
+        $editButton.siblings('.skillName').hide();
+        $editButton.siblings('.deleteSkillButton').hide();
+        $editButton.siblings('.saveSkillButton').show();
+        $editButton.siblings('.cancelEditButton').show();
+      });
+
+      $skillsUl.on('click', '.cancelEditButton', function () {
+        const $cancelEditButton = $(this).hide();
+
+        $cancelEditButton
+          .siblings('input[name^="skill-"]')
+          .attr('type', 'hidden');
+        $cancelEditButton.siblings('.skillName').show();
+        $cancelEditButton.siblings('.saveSkillButton').hide();
+        $cancelEditButton.siblings('.deleteSkillButton').show();
+        $cancelEditButton.siblings('.editSkillButton').show();
+      });
+
+      $skillsUl.on('click', '.saveEditButton', function () {
+        const $saveSkillButton = $(this).hide();
+
+        const newValue = $saveSkillButton
+          .siblings('input[name^="skill-"]')
+          .attr('type', 'hidden')
+          .val();
+        $saveSkillButton.siblings('.skillName').text(newValue).show();
+        $saveSkillButton.siblings('.saveSkillButton').hide();
+        $saveSkillButton.siblings('.deleteSkillButton').show();
+        $saveSkillButton.siblings('.editSkillButton').show();
+        $saveSkillButton.siblings('.cancelEditButton').hide();
+      });
+
+      $skillsUl.appendTo($parentFieldset);
+    }
+
+    const $skillLi = $('<li>');
+
+    $('<span>', {
+      class: 'skillName',
+      text: skillName,
+    }).appendTo($skillLi);
+
+    const $skillHiddenInput = $('<input>', {
+      type: 'hidden',
+      name: `skill-${skillName}`,
+      value: skillName,
+    });
+
+    // add input to li
+    $skillLi.append($skillHiddenInput);
+
+    //add delete button
+    $('<button>', {
+      type: 'button',
+      text: '-',
+      class: 'deleteSkillButton',
+    }).appendTo($skillLi);
+
+    //add edit button
+    $('<button>', {
+      type: 'button',
+      text: 'Edit',
+      class: 'editSkillButton',
+    }).appendTo($skillLi);
+
+    //add save button
+    $('<button>', {
+      type: 'button',
+      text: 'save',
+      class: 'saveSkillButton',
+    })
+      .hide()
+      .appendTo($skillLi);
+
+    //add cancel button
+    $('<button>', {
+      type: 'button',
+      text: 'Cancel',
+      class: 'cancelEditButton',
+    })
+      .hide()
+      .appendTo($skillLi);
+
+    // the real DOM
+    $skillsUl.append($skillLi);
+
+    // empty original element
+    $skillInput.val('');
   });
 
   // hoisting
   function renderPerson(person) {
-    const $personContainer = $('<article>');
+    const $personContainer = $('<article>', {
+      class: 'personDetails',
+    });
 
     $('<h1>', {
       text: `${person.name} ${person.surname}`,
@@ -36,6 +173,36 @@ $(function () {
       text: `Varsta: ${person.age}`,
     }).appendTo($personContainer);
 
+    let $skillsUl;
+    if (person.skills.length > 0) {
+      $skillsUl = renderSkillsUl(person.skills);
+    }
+
+    $personContainer.append($skillsUl);
+
     return $personContainer;
+  }
+
+  function renderSkillsUl(skillsArray) {
+    const $skillsUl = $('<ul>');
+
+    skillsArray.forEach(function (skillName) {
+      $('<li>', {
+        text: skillName,
+      }).appendTo($skillsUl);
+    });
+
+    return $skillsUl;
+  }
+
+  function resetForm($form) {
+    const $nameInputs = $form.find('[name]');
+
+    $nameInputs.each(function () {
+      // in function functions, this = the DOM element
+      const $input = $(this);
+
+      $input.val('');
+    });
   }
 });
