ebd0a1c8976f96fdbc101960b0a4b5664f3bb9a2
Muss86 <florin.gruianu@gmail.com>
2021-11-15 22:06:25 +0200

    15noiembrie

---
+++ M02S03/ex04/app.js
@@ -0,0 +1,16 @@
+// load image & paragraph element from DOM
+const imageElement = document.getElementsByTagName('img')[0];
+const messageElement = document.querySelector('.message');
+
+console.log(imageElement.complete);
+// bind event to image
+// imageElement.addEventListener('load', () => {
+//   messageElement.textContent = 'Imaginea s-a incarcat.';
+// });
+
+// v2
+imageElement.onload = () => {
+  messageElement.textContent = 'Imaginea s-a incarcat a doua oara.';
+
+  console.log(imageElement.complete);
+};
