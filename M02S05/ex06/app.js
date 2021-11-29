d598c405cbaa191c494a765b835991b0d3bafc14
Muss86 <florin.gruianu@gmail.com>
2021-11-29 22:06:25 +0200

    caruseluri

---
+++ M02S05/ex06/app.js
@@ -0,0 +1,17 @@
+$(function () {
+  const $dialog = $('#dialog').dialog({
+    autoOpen: false,
+  });
+
+  const altAlert = (message) => {
+    $dialog.find('p').text(message).end().dialog('open');
+  };
+
+  alert('in alert');
+
+  window.alert = altAlert;
+
+  alert('in jquery ui');
+
+  // $dialog.dialog('open');
+});
